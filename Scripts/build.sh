4b74beb2bd10d23befd3664c38c47168d96bf439
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-19 22:08:11 -0500

    fix typo

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,7 +8,7 @@ CompilerOptions="-o main"  # Output file will be named "main"
 CompilerOptions+=" -Wall -Wextra -Werror"  # Turn on more compiler warnings 
                                            # and treat them as errors
 CompilerOptions+=" -std=c++23"   # Compile with C++23 standard                                           
-CompilerOptions+=" -I../Src/
+CompilerOptions+=" -I../Src/"    # Set include directory
 clang++  ../Test/test_utest.cpp ../Src/unittest.cpp $CompilerOptions
 
 cd ..

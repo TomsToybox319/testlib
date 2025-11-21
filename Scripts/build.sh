3f2094b9fc3e332b4522095d216a37b393b1f497
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-21 18:47:38 -0500

    more warnings

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,6 +8,7 @@ CompilerOptions="-o main"  # Output file will be named "main"
 CompilerOptions+=" -Wall  -Werror "  # Turn on more compiler warnings and treat them as errors
 CompilerOptions+=" -Weverything" # Warn all of the things!
 CompilerOptions+=" -Wno-c++98-compat" # Don't need C++98 comptability
+CompilerOptions+=" -Wno-padded" # Don't care about padding
 CompilerOptions+=" -std=c++23"   # Compile with C++23 standard                                           
 CompilerOptions+=" -I../Src/"    # Set include directory
 clang++  ../Test/test_utest.cpp ../Src/unittest.cpp $CompilerOptions

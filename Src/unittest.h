edc0ce014e4bc0a5cdc79fa665955ff955f8384a
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-21 18:49:51 -0500

    warnings

--- Src/unittest.h
+++ Src/unittest.h
@@ -131,12 +131,12 @@ class test_runner
     {
     }
 
-    constexpr result(bool Passed, std::string Message, size_t TestsFailed,
-                     size_t TestsPassed)
-        : Passed(Passed),
-          Message(Message),
-          TestsFailed(TestsFailed),
-          TestsPassed(TestsPassed)
+    constexpr result(bool PassedArg, std::string MessageArg,
+                     size_t TestsFailedArg, size_t TestsPassedArg)
+        : Passed(PassedArg),
+          Message(std::move(MessageArg)),
+          TestsFailed(TestsFailedArg),
+          TestsPassed(TestsPassedArg)
     {
     }
 

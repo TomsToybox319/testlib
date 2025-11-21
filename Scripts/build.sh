17ab39c31c945d9af72d60cf6d3799ed27442d75
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-21 18:43:38 -0500

    remove c++98 compat

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,9 +5,9 @@ mkdir -p Build
 cd Build
 
 CompilerOptions="-o main"  # Output file will be named "main"
-CompilerOptions+=" -Wall -Wextra -Werror -Wconversion -Wshadow -Wnon-virtual-dtor -pedantic"  # Turn on more compiler warnings 
+CompilerOptions+=" -Wall  -Werror "  # Turn on more compiler warnings and treat them as errors
 CompilerOptions+=" -Weverything" # Warn all of the things!
-                                           # and treat them as errors
+CompilerOptions+=" -Wno-c++98-compat" # Don't need C++98 comptability
 CompilerOptions+=" -std=c++23"   # Compile with C++23 standard                                           
 CompilerOptions+=" -I../Src/"    # Set include directory
 clang++  ../Test/test_utest.cpp ../Src/unittest.cpp $CompilerOptions

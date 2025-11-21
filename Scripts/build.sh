8f66897edadb825465ca9790d3e601b2f364dea1
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-21 18:52:31 -0500

    warnings

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -10,6 +10,7 @@ CompilerOptions+=" -Weverything" # Warn all of the things!
 CompilerOptions+=" -Wno-c++98-compat" # Don't need C++98 comptability
 CompilerOptions+=" -Wno-padded" # Don't care about padding
 CompilerOptions+=" -Wno-global-constructors" # We need global ctors
+CompilerOptions+=" -Wno-exit-time-destructors" # We need exit-time dtors
 CompilerOptions+=" -std=c++23"   # Compile with C++23 standard                                           
 CompilerOptions+=" -I../Src/"    # Set include directory
 clang++  ../Test/test_utest.cpp ../Src/unittest.cpp $CompilerOptions
